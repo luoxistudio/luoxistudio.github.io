3279a2cf39c5bf08354a353f060519ea312634c0
Chengqi Zhu <chengqizhufm@gmail.com>
2019-10-24 23:09:58 -0700

    Update GA tracking

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,19 +1,46 @@
-import React, { Component } from 'react';
-import Icon from '../Icon';
-
+import React, { Component } from "react";
+import Icon from "../Icon";
+import ReactGA from "react-ga";
 
 class Contact extends Component {
-	render() {
-        const classes = this.props.className ? 'Contact ' + this.props.className : 'Contact';
-		return (
-            <div className={classes}>
-                <a className="Contact__icon-link" href="https://www.weibo.com/jeezu" target="_blank" rel="noopener noreferrer"><Icon iconName="weibo" /></a>
-                {/* <a className="Contact__icon-link" href="javascript:void();"><Icon iconName="wechat" /></a>
+  _handleContactClick = event => {
+    const contactMethod = event.currentTarget.dataset.method;
+    ReactGA.event({
+      category: "Contact",
+      action: "Click",
+      label: contactMethod
+    });
+  };
+
+  render() {
+    const classes = this.props.className ? "Contact " + this.props.className : "Contact";
+    return (
+      <div className={classes}>
+        <a
+          className="Contact__icon-link"
+          href="https://www.weibo.com/jeezu"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-method="Weibo"
+          onClick={this._handleContactClick}
+        >
+          <Icon iconName="weibo" />
+        </a>
+        {/* <a className="Contact__icon-link" href="javascript:void();"><Icon iconName="wechat" /></a>
                 <a className="Contact__icon-link" href="javascript:void();"><Icon iconName="instagram" /></a> */}
-                <a className="Contact__icon-link" href="mailto:229465032@qq.com" target="_blank" rel="noopener noreferrer"><Icon iconName="email" /></a>
-            </div>    
-		);
-	}
+        <a
+          className="Contact__icon-link"
+          data-method="Email"
+          href="mailto:229465032@qq.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={this._handleContactClick}
+        >
+          <Icon iconName="email" />
+        </a>
+      </div>
+    );
+  }
 }
 
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.scss';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.scss";
+import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Google Analytics
+ReactGA.initialize("UA-75265785-3");
+ReactGA.pageview("/");
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
